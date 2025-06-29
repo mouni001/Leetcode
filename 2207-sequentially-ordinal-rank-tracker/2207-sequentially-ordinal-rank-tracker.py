6aeddb31df6cd9e074b77435312162f4a7972cc0
MouniraZitouni <71095730+mouni001@users.noreply.github.com>
2025-06-29 18:12:01 -0400

    Time: 463 ms (12.95%), Space: 41.2 MB (65.18%) - LeetHub

---
+++ 2207-sequentially-ordinal-rank-tracker/2207-sequentially-ordinal-rank-tracker.py
@@ -0,0 +1,43 @@
+class SORTracker:
+
+    def __init__(self):
+        self.location = []
+        self.query_count = 0
+        
+
+    def add(self, name: str, score: int) -> None:
+        location = (-score, name)
+
+        left, right = 0, len(self.location)
+        while left < right:
+            mid = (left + right) //2
+
+            if self.location[mid] <= location:
+                left = mid + 1
+            else:
+                right = mid
+
+        self.location.insert(left, location)
+
+    def get(self) -> str:
+        result = self.location[self.query_count][1]
+        self.query_count += 1
+        return result
+
+tracker = SORTracker()
+tracker.add("bradford", 2)
+tracker.add("bradford", 3)
+print(tracker.get())
+tracker.add("alps", 2)
+print(tracker.get())
+tracker.add("orland", 2)
+print(tracker.get())
+
+
+        
+
+
+# Your SORTracker object will be instantiated and called as such:
+# obj = SORTracker()
+# obj.add(name,score)
+# param_2 = obj.get()
